8c062da6bf041828bf5f3f1a1e2bc05051d15693
davidcoats <david@getsafetycone.com>
2024-01-09 14:33:37 -0800

    Initial commit.

---
+++ source/R5T.T0231/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0231
+{
+	/// <summary>
+	/// File system paths related, root, location-aware strong-types using the string underlying type.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0231/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0231
+File system paths related, root, location-aware strong-types using the string underlying type.
